48399f91a572e6f039e2623f1b90d211509fee72
Shane Thacker <shane@steadymade.com>
2022-02-14 14:55:56 -0600

    Make more precise

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ const intervals = [
 module.exports = (d, withAdverb = true) => {
   const date = typeof d === 'object' ? d : new Date(d);
   const diff = Date.now() - date.getTime();
-  const isPast = diff > 0;
-  const seconds = Math.floor(Math.abs(diff) / 1000);
+  const isPast = diff >= 0;
+  const seconds = Math.round(Math.abs(diff) / 1000);
   const interval = intervals.find(i => i.seconds <= seconds);
-  const count = interval.seconds ? Math.floor(seconds / interval.seconds) : 1;
+  const count = interval.seconds
+    ? Math.round(seconds / interval.seconds)
+    : seconds
+      ? seconds
+      : 1;
   const output = `${count} ${interval.label}${count !== 1 ? 's' : ''}`;
   return !withAdverb
     ? output
